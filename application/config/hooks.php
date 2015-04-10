f6977dcd423a0d82c4d2160d830ff6c5d46a9cc6
duniadua <duniadua@gmail.com>
2015-04-10 17:37:30 +0700

    Set Config hooks

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -9,7 +9,12 @@
 |	http://codeigniter.com/user_guide/general/hooks.html
 |
 */
-
+$hook['post_controller_constructor'] = array(
+    'class' => 'LanguageLoader',
+    'function' => 'initialize',
+    'filename' => 'LanguageLoader.php',
+    'filepath' => 'hooks'
+);
 
 
 /* End of file hooks.php */
